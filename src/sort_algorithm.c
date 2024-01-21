b270799076a8d98cdb4c086f16aa7c96d263002a
Omar Zini <omarzini82@gmail.com>
2024-01-21 15:47:11 +0100

    reduccion lineas funcion debajo 25

--- src/sort_algorithm.c
+++ src/sort_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: ozini <ozini@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 13:28:26 by ozini             #+#    #+#             */
-/*   Updated: 2024/01/21 15:40:10 by cosorio-         ###   ########.fr       */
+/*   Updated: 2024/01/21 15:46:09 by ozini            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,14 @@ void	get_target_node(t_node *stack_a, t_node *stack_b)
 	t_node	*tmp_b;
 	t_node	*target_node;
 	int		closest;
-	int		i;
 
-	i = 1;
 	tmp_b = stack_b;
 	while (tmp_b)
 	{
-		target_node = NULL;
 		closest = INT_MAX;
 		tmp_a = stack_a;
 		while (tmp_a != NULL)
 		{
-			//printf("Bucle %d", i);
-			i++;
 			if (tmp_b->value < tmp_a->value && tmp_a->value < closest)
 			{
 				closest = tmp_a->value;
@@ -38,14 +33,9 @@ void	get_target_node(t_node *stack_a, t_node *stack_b)
 			}
 			tmp_a = tmp_a->next;
 		}
-		if (!target_node)
-		{
-			//printf("Entra");
+		if (closest == INT_MAX)
 			target_node = get_min(stack_a);
-		}
-		//else
 		tmp_b->target_node = target_node;
-		//printf("El target node es %d", tmp_b->target_node->value);
 		tmp_b = tmp_b->next;
 	}
 
@@ -55,20 +45,17 @@ t_node *get_min(t_node *stack)
 {
 	int min;
 	t_node *temp_min;
-	t_node *t;
 
 	min = INT_MAX;
 	temp_min = NULL;
-	t = stack;
-	while (t)
+	while (stack)
 	{
-		//printf("El nodo de A vale %d\n", t->value);
-		if (t->value < min)
+		if (stack->value < min)
 		{
-			min = t->value;
-			temp_min = t;
+			min = stack->value;
+			temp_min = stack;
 		}
-		t = t ->next;
+		stack = stack ->next;
 	}
 	return (temp_min);
 }
